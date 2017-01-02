0c9a5f337f489c918ead2cd64ae465332611f66b
Russell Gallop <russell.gallop@gmail.com>
2017-01-02 20:17:30 +0000

    Rewrite unit tests with googletest.

--- test2048.cpp
+++ test2048.cpp
@@ -1,92 +1,91 @@
-#include <stdio.h>
-#include <assert.h>
+#include "gtest/gtest.h"
 #include "2048.h"
 
-void compare_board(int a[SIZE * SIZE], int b[SIZE * SIZE]) {
-    printf("Comparing board\n");
-    for (int i = 0; i != SIZE * SIZE; i++) {
-        if (a[i] != b[i]) {
-            printf("Got: \n");
-            print_board(a);
-            printf("Expected: \n");
-            print_board(b);
-            assert(false);
-            break;
-        }
-        //assert(a[i] == b[i]);
-    }
-}
-
-void compare_array(int a[SIZE], int b[SIZE]) {
-    printf("Comparing array\n");
-    for (int i = 0; i != SIZE; i++) {
-        if (a[i] != b[i]) {
-            printf("Got: ");
-            print_array(a);
-            printf("Expected: ");
-            print_array(b);
-            assert(false);
-            break;
-        }
-        //assert(a[i] == b[i]);
-    }
-}
-
-void test_combine() {
-    printf("test combine\n");
-    int row[SIZE] = {2, 2, 4, 4};
-    int expected[SIZE] = {4, 8, 0, 0};
-    int score = combine(row);
-    compare_array(row, expected);
-    assert(score == 12);
-}
-
-void test_reverse() {
-    printf("test reverse\n");
+TEST(TestReverse, Simple) {
     int row[SIZE] = {1, 2, 4, 8};
     int expected[SIZE] = {8, 4, 2, 1};
     reverse(row);
-    compare_array(row, expected);
+    for (int i = 0; i != SIZE; i++) {
+        EXPECT_EQ(expected[i], row[i]);
+    }
 }
 
-void test_shift() {
-    printf("test shift\n");
+TEST(TestCombine, Simple) {
+    int row[SIZE] = {2, 2, 4, 4};
+    int expected[SIZE] = {4, 8, 0, 0};
+    int score = combine(row);
+    int expected_score = 12;
+    for (int i = 0; i != SIZE; i++) {
+        EXPECT_EQ(expected[i], row[i]);
+    }
+    EXPECT_EQ(expected_score, score);
+}
+
+TEST(TestShift, Left) {
     int row[SIZE] = {0, 2, 2, 2};
     int expected[SIZE] = {4, 2, 0, 0};
     int score = shift(row, 0);
-    compare_array(row, expected);
-    assert(score == 4);
-    int row2[SIZE] = {0, 2, 2, 8};
-    int expected2[SIZE] = {0, 0, 4, 8};
-    int score2 = shift(row2, 1);
-    compare_array(row2, expected2);
-    assert(score2 == 4);
+    int expected_score = 4;
+    for (int i = 0; i != SIZE; i++) {
+        EXPECT_EQ(expected[i], row[i]);
+    }
+    EXPECT_EQ(expected_score, score);
 }
 
-void test_move() {
-    printf("test move\n");
-    // shift left
-    int board[SIZE * SIZE] = {0, 2, 0, 4, 2, 2, 8, 0, 2, 2, 2, 8, 2, 2, 4, 4};
-    print_board(board);
-    int expected[SIZE * SIZE] = {2, 4, 0, 0, 4, 8, 0, 0, 4, 2, 8, 0, 4, 8, 0, 0};
+TEST(TestShift, Right) {
+    int row[SIZE] = {0, 2, 2, 8};
+    int expected[SIZE] = {0, 0, 4, 8};
+    int score = shift(row, 1);
+    int expected_score = 4;
+    for (int i = 0; i != SIZE; i++) {
+        EXPECT_EQ(expected[i], row[i]);
+    }
+    EXPECT_EQ(expected_score, score);
+}
+
+TEST(TestMove, Left) {
+    int board[SIZE * SIZE] = {
+        0, 2, 0, 4,
+        2, 2, 8, 0,
+        2, 2, 2, 8,
+        2, 2, 4, 4
+    };
+    int expected[SIZE * SIZE] = {
+        2, 4, 0, 0,
+        4, 8, 0, 0,
+        4, 2, 8, 0,
+        4, 8, 0, 0
+    };
     int score = move(board, 3);
-    compare_board(board, expected);
-    assert(score == 20);
-    // shift up
-    int board2[SIZE * SIZE] = {0, 2, 0, 4, 2, 2, 8, 0, 2, 2, 2, 8, 2, 2, 4, 4};
-    print_board(board2);
-    print_board(board2);
-    print_board(board2);
-    int expected2[SIZE * SIZE] = {4, 4, 8, 4, 2, 4, 2, 8, 0, 0, 4, 4, 0, 0, 0, 0};
-    int score2 = move(board2, 0);
-    compare_board(board2, expected2);
-    assert(score2 == 12);
+    int expected_score = 20;
+    for (int i = 0; i != SIZE * SIZE; i++) {
+        EXPECT_EQ(expected[i], board[i]);
+    }
+    EXPECT_EQ(expected_score, score);
 }
 
-int main() {
-    test_reverse();
-    test_combine();
-    test_shift();
-    test_move();
-    return 0;
+TEST(TestMove, Up) {
+    int board[SIZE * SIZE] = {
+        0, 2, 0, 4,
+        2, 2, 8, 0,
+        2, 2, 2, 8,
+        2, 2, 4, 4
+    };
+    int expected[SIZE * SIZE] = {
+        4, 4, 8, 4,
+        2, 4, 2, 8,
+        0, 0, 4, 4,
+        0, 0, 0, 0
+    };
+    int score = move(board, 0);
+    int expected_score = 12;
+    for (int i = 0; i != SIZE * SIZE; i++) {
+        EXPECT_EQ(expected[i], board[i]);
+    }
+    EXPECT_EQ(expected_score, score);
+}
+
+int main(int argc, char **argv) {
+    ::testing::InitGoogleTest(&argc, argv);
+    return RUN_ALL_TESTS();
 }
