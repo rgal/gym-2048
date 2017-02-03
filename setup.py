f5af94f9b043409629ad8bb3056cf6e6d981370d
Icyblade Dai <icyblade@users.noreply.github.com>
2017-02-03 16:12:42 +0800

    Minor bug fix
    Fix "cannot import name envs" bug

--- setup.py
+++ setup.py
@@ -3,5 +3,5 @@ from setuptools import setup
 setup(name='gym_2048',
       version='0.0.1',
       install_requires=['gym'],
-      packages=['gym_2048'],
+      packages=['gym_2048', 'gym_2048.envs'],
 )
