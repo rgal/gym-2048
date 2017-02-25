08eedeb086a8bb8ef17686ae5cb32b0e2cb09c2a
Russell Gallop <russell.gallop@gmail.com>
2017-02-25 13:42:03 +0000

    Add help for epsilon argument

--- play.py
+++ play.py
@@ -88,7 +88,7 @@ class Knowledge(object):
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
-    parser.add_argument('--epsilon', type=float, default=0.95)
+    parser.add_argument('--epsilon', type=float, default=0.95, help="Epsilon is probability of using best action")
     parser.add_argument('--episodes', type=int, default=10000)
     parser.add_argument('--reportfrequency', type=int, default=100)
     parser.add_argument('-o', '--output', default=None)
