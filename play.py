ffe76341c23d33bde45b3f5c5693e2582c7410a3
Russell Gallop <russell.gallop@gmail.com>
2017-02-25 22:13:38 +0000

    Fix meaning of epsilon. Should be exploration value.

--- play.py
+++ play.py
@@ -89,7 +89,7 @@ class Knowledge(object):
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
     parser.add_argument('--alpha', type=float, default=0.1, help="Alpha is the proportion to update your estimate by")
-    parser.add_argument('--epsilon', type=float, default=0.95, help="Epsilon is probability of using best action")
+    parser.add_argument('--epsilon', type=float, default=0.1, help="Epsilon is probability of exploration rather than choosing best action")
     parser.add_argument('--lambda', dest='llambda', type=float, default=0.9, help="Lambda is the decay value for return on action")
     parser.add_argument('--episodes', type=int, default=10000)
     parser.add_argument('--reportfrequency', type=int, default=100)
@@ -127,7 +127,7 @@ if __name__ == '__main__':
             #env.render()
             #print(observation)
             action = None
-            if (random.uniform(0, 1) < epsilon):
+            if (random.uniform(0, 1) > epsilon):
                 if knowledge.get_node_for_state(tuple(observation)):
                     #print "Picking best known action"
                     state_node = knowledge.get_node_for_state(tuple(observation))
