1537786a688d6a5b84ace776db62cb634529a776
Russell Gallop <russell.gallop@gmail.com>
2017-02-25 15:21:30 +0000

    Fix how TD lambda estimates are calculated. Should be scaled by 1 - lambda.

--- play.py
+++ play.py
@@ -155,14 +155,16 @@ if __name__ == '__main__':
         # Go through history creating or updating knowledge
         # Calculate TD lambda estimates, actual value + lambda of next one plus lambda squared of the next one etc.
         td_lambda_estimate = list()
-        # TD0 lambda estimate
+        # TD lambda estimate (First estimate should be weighted by (1 - l), second by (1 - l)l etc.
         #print len(history)
         for h in history:
-            td_lambda_estimate.append(h[2])
+            td_factor = 0
+            lambda_multiplier = (1. - llambda) * (llambda**td_factor)
+            td_lambda_estimate.append(h[2] * lambda_multiplier)
         # Add TD1 (e.g. Lambda * reward goes to n-1 action
         for td_factor in range(1, min(10, len(history))):
             #print "Lambda estimate: {}".format(td_lambda_estimate)
-            lambda_multiplier = llambda**td_factor
+            lambda_multiplier = (1. - llambda) * (llambda**td_factor)
             #print "Lambda multiplier: {}".format(lambda_multiplier)
             for i in range(len(history) - td_factor):
                 td_lambda_estimate[i] += history[i+td_factor][2] * lambda_multiplier
