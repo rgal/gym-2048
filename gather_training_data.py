f6728cd40be4da97c80c48827c3b07a9015d3092
Russell Gallop <russell.gallop@gmail.com>
2018-01-15 23:04:29 +0000

    Add gather_training_data.py for collecting data for training

---
+++ gather_training_data.py
@@ -0,0 +1,81 @@
+"""Gather training data from the game"""
+
+from __future__ import print_function
+
+import numpy as np
+import pygame
+
+import gym
+
+import gym_2048
+
+def gather_training_data(env, seed=None):
+    """Gather training data from letting the user play the game"""
+    # Data is a list of input and outputs
+    data = list()
+    # Initialise seed for environment
+    if seed:
+        env.seed(seed)
+    else:
+        env.seed()
+    observation = env.reset()
+    print("User cursor keys to play, q to quit")
+    for t in range(10):
+        action = None
+        env.render()
+        # Ask user for action
+        event = pygame.event.wait()
+        if event.type == pygame.KEYDOWN:
+            if event.key == pygame.K_UP:
+                action = 0
+            if event.key == pygame.K_RIGHT:
+                action = 1
+            if event.key == pygame.K_DOWN:
+                action = 2
+            if event.key == pygame.K_LEFT:
+                action = 3
+            if event.key == pygame.K_q:
+                break
+        # Read and discard the keyup event
+        event = pygame.event.wait()
+        print("Read action {}".format(action))
+
+        # Add this data to the data collection
+        data.append({'input': observation, 'output': action})
+        observation, reward, done, info = env.step(action)
+        print()
+
+        if done:
+            break
+
+    return data
+
+if __name__ == '__main__':
+    # Initialise environment
+    env = gym.make('2048-v0')
+
+    # Initialise pygame for detecting keypresses
+    pygame.init()
+
+    data = gather_training_data(env)
+
+    # Close the environment
+    env.close()
+
+    data_size = len(data)
+    print("Got {} data values".format(data_size))
+    x = np.empty([data_size, 16], dtype=np.int)
+    for idx, d in enumerate(data):
+        x[idx] = d['input']
+    y = np.zeros([data_size, 4], dtype=np.int)
+    for idx, d in enumerate(data):
+        y[idx, d['output']] = 1
+
+    print("Inputs: {}".format(x))
+    print("Outputs: {}".format(y))
+
+    # Save training data
+    with open('x.npy', 'w') as f:
+        np.save(f, x)
+    with open('y.npy', 'w') as f:
+        np.save(f, y)
