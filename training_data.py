95462ad392b453debd4b9021fa49cbe5b51bbbb4
Russell Gallop <russell.gallop@gmail.com>
2018-02-05 22:43:28 +0000

    Reimplement training data using numpy arrays

--- training_data.py
+++ training_data.py
@@ -8,32 +8,31 @@ import os
 class training_data(object):
 
     def __init__(self):
-        self._data = list()
+        self._x = np.empty([0, 4, 4], dtype=np.int)
+        self._y = np.zeros([0, 4], dtype=np.int)
 
     def add(self, board, action):
-        self._data.append({'input': board, 'output': action})
+        self._x = np.append(self._x, np.reshape(board, (1, 4, 4)), axis=0)
+        y = np.zeros([1, 4], dtype=np.int)
+        y[0, action] = 1
+        self._y = np.append(self._y, y, axis=0)
 
     def size(self):
-        return len(self._data)
+        return self._x.shape[0]
 
     def write(self, output_dir):
-        x = np.empty([self.size(), 4, 4], dtype=np.int)
-        for idx, d in enumerate(self._data):
-            x[idx] = np.reshape(d['input'], (4, 4))
-        y = np.zeros([self.size(), 4], dtype=np.int)
-        for idx, d in enumerate(self._data):
-            y[idx, d['output']] = 1
-
-
         # Save training data
         try:
             os.makedirs(output_dir)
         except OSError:
             pass
         with open(os.path.join(output_dir, 'x.npy'), 'w') as f:
-            np.save(f, x)
+            print("Outputting {} with shape {}".format('x.npy', self._x.shape))
+            np.save(f, self._x)
         with open(os.path.join(output_dir, 'y.npy'), 'w') as f:
-            np.save(f, y)
+            print("Outputting {} with shape {}".format('y.npy', self._y.shape))
+            np.save(f, self._y)
 
     def dump(self):
-        print(self._data)
+        print(self._x)
+        print(self._y)
