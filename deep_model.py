37fb5d944ea5a5e7807ea9df5a1da08d5a38c984
Russell Gallop <russell.gallop@gmail.com>
2018-11-14 20:38:53 +0000

    Increase batch size used by numpy_train_fn()

--- deep_model.py
+++ deep_model.py
@@ -7,7 +7,11 @@ def numpy_predict_fn(observation):
    return tf.estimator.inputs.numpy_input_fn(x={'board': observation.reshape((-1,4,4,1)).astype(np.float32)}, num_epochs=1, shuffle=False)
 
 def numpy_train_fn(observation, action, reward):
-   return tf.estimator.inputs.numpy_input_fn(x={'board': observation.reshape((-1,4,4,1)).astype(np.float32)}, y={'action': action.astype(np.int32), 'reward': reward}, num_epochs=1, shuffle=False)
+   return tf.estimator.inputs.numpy_input_fn(x={'board': observation.reshape((-1,4,4,1)).astype(np.float32)},
+       y={'action': action.astype(np.int32), 'reward': reward},
+       batch_size=1024,
+       num_epochs=1,
+       shuffle=True)
 
 def my_input_fn(file_path, perform_shuffle=False, repeat_count=1, augment=False, batch_size=32):
    def decode_csv(line):
