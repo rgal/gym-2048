932aff6ebbcda04a630ecf2a792ae1737930c961
Russell Gallop <russell.gallop@gmail.com>
2018-11-28 21:37:00 +0000

    Change to single output for quality with action as input to fully connected layer

--- deep_model.py
+++ deep_model.py
@@ -21,25 +21,25 @@ def my_input_fn(file_path, perform_shuffle=False, repeat_count=1, augment=False,
        features = tf.reshape(tf.cast(tf.stack(features), tf.float32), [4, 4, 1])
        action = parsed_line[16]
        reward = parsed_line[17]
-       return {'board': features}, {'action': action, 'reward': reward}
+       return {'board': features, 'action': action}, {'action': action, 'reward': reward}
 
    def hflip(feature, label):
        image = feature['board']
        flipped_image = tf.image.flip_left_right(image)
        #tf.Print(flipped_image, [image, flipped_image], "Image and flipped left right")
-       newlabel = tf.gather([0, 3, 2, 1], label['action'])
+       new_action = tf.gather([0, 3, 2, 1], label['action'])
        #tf.Print(newlabel, [label['action'], newlabel], "Label and flipped left right")
-       return {'board': flipped_image}, {'action': newlabel, 'reward': label['reward']}
+       return {'board': flipped_image, 'action': new_action}, {'action': new_action, 'reward': label['reward']}
 
    def rotate_board(feature, label, k):
        image = feature['board']
        rotated_image = tf.image.rot90(image, 4 - k)
        #tf.Print(rotated_image, [image, rotated_image], "Image and rotated by k={}".format(k))
-       newlabel = label['action']
-       newlabel += k
-       newlabel %= 4
-       #tf.Print(newlabel, [label['action'], newlabel], "Label and rotated by k={}".format(k))
-       return {'board': rotated_image}, {'action': newlabel, 'reward': label['reward']}
+       new_action = label['action']
+       new_action += k
+       new_action %= 4
+       #tf.Print(new_action, [label['action'], new_action], "Label and rotated by k={}".format(k))
+       return {'board': rotated_image, 'action': new_action}, {'action': new_action, 'reward': label['reward']}
 
    def rotate90(feature, label):
        return rotate_board(feature, label, 1)
@@ -159,7 +159,13 @@ def my_model(features, labels, mode, params):
     # Flatten into a batch of vectors
     # Input shape: [batch_size, 4, 4, 16]
     # Output shape: [batch_size, 4 * 4 * 16]
-    net = tf.reshape(block_inout, [-1, 4 * 4 * params['filters']])
+    net_out = tf.reshape(block_inout, [-1, 4 * 4 * params['filters']])
+
+    # Get actions to be used as inputs. This in encoded 0-3
+    action_inputs = features['action']
+
+    # Concatenate action inputs with the output of the convolutional network
+    net = tf.concat([net_out, tf.cast(tf.one_hot(action_inputs, 4), tf.float32)], 1)
 
     for units in params['fc_layers']:
         # Fully connected layer
@@ -172,48 +178,57 @@ def my_model(features, labels, mode, params):
             inputs=net, rate=params['dropout_rate'], training=mode == tf.estimator.ModeKeys.TRAIN)
 
     # Compute logits (1 per class).
-    logits = tf.layers.dense(net, params['n_classes'], activation=None)
+    logits = tf.layers.dense(net, 1, activation=None)
 
     # Compute predictions.
-    predicted_classes = tf.argmax(logits, 1)
+    #predicted_classes = tf.argmax(logits, 1)
     if mode == tf.estimator.ModeKeys.PREDICT:
         predictions = {
-            'class_ids': predicted_classes[:, tf.newaxis],
-            'probabilities': tf.nn.softmax(logits),
+            #'class_ids': predicted_classes[:, tf.newaxis],
+            #'probabilities': tf.nn.softmax(logits),
             'logits': logits,
         }
         return tf.estimator.EstimatorSpec(mode, predictions=predictions)
 
-    # Compute loss on actions compared to records
-    action_labels = labels['action'] # Shape [batch_size, 1]
-    # Then calculate loss with softmax cross entropy
-    action_loss = tf.losses.sparse_softmax_cross_entropy(labels=action_labels, logits=logits)
+#    # Compute loss on actions compared to records
+#    action_labels = labels['action'] # Shape [batch_size, 1]
+#    # Then calculate loss with softmax cross entropy
+#    action_loss = tf.losses.sparse_softmax_cross_entropy(labels=action_labels, logits=logits)
+#
+#    # Compute loss based no rewards compared to those from environment
+#    reward_labels = labels['reward'] # Shape [batch_size, 1]
+#
+#    # Only have reward from environment from one action so gather predicted
+#    # rewards for those actions and calculate MSE compared to environment
+#    batch_size = tf.shape(labels['action'])[0]
+#    action_idx = tf.reshape(tf.range(batch_size),[-1, 1])
+#    action_labels_reshaped = tf.reshape(labels['action'],[-1, 1])
+#    action_reshaped = tf.reshape(tf.concat([action_idx, action_labels_reshaped],axis=1), [-1, 1, 2])
+#    gathered_logits = tf.gather_nd(logits, action_reshaped)
+#    reward_loss = tf.losses.mean_squared_error(tf.reshape(reward_labels, [-1, 1]), gathered_logits)
 
-    # Compute loss based no rewards compared to those from environment
-    reward_labels = labels['reward'] # Shape [batch_size, 1]
+    # Batch normalize to get distribution closer to zero
+    reward_bn = tf.layers.batch_normalization(
+        inputs=tf.reshape(labels['reward'], [-1, 1]),
+        training=mode == tf.estimator.ModeKeys.TRAIN
+    )
 
-    # Only have reward from environment from one action so gather predicted
-    # rewards for those actions and calculate MSE compared to environment
-    batch_size = tf.shape(labels['action'])[0]
-    action_idx = tf.reshape(tf.range(batch_size),[-1, 1])
-    action_labels_reshaped = tf.reshape(labels['action'],[-1, 1])
-    action_reshaped = tf.reshape(tf.concat([action_idx, action_labels_reshaped],axis=1), [-1, 1, 2])
-    gathered_logits = tf.gather_nd(logits, action_reshaped)
-    reward_loss = tf.losses.mean_squared_error(tf.reshape(reward_labels, [-1, 1]), gathered_logits)
+    # Calculate Q loss on the Q of the input action
+    q_loss = tf.losses.mean_squared_error(reward_bn, tf.reshape(logits, [-1, 1]))
 
     # Select loss (action or reward)
-    loss = reward_loss
+    loss = q_loss
 
     # Compute evaluation metrics.
-    accuracy = tf.metrics.accuracy(labels=action_labels,
-                                   predictions=predicted_classes,
-                                   name='acc_op')
-    metrics = {'accuracy': accuracy}
-    tf.summary.scalar('accuracy', accuracy[1])
+#    accuracy = tf.metrics.accuracy(labels=action_labels,
+#                                   predictions=predicted_classes,
+#                                   name='acc_op')
+#    metrics = {'accuracy': accuracy}
+#    tf.summary.scalar('accuracy', accuracy[1])
 
     if mode == tf.estimator.ModeKeys.EVAL:
         return tf.estimator.EstimatorSpec(
-            mode, loss=loss, eval_metric_ops=metrics)
+            mode, loss=loss)
 
     # Create training op.
     assert mode == tf.estimator.ModeKeys.TRAIN
