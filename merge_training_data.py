fd4c6e0bb7c672d75b83c89b8fe18382fdd90958
Russell Gallop <russell.gallop@gmail.com>
2018-12-08 22:39:13 +0000

    Merge training_data with next state

--- merge_training_data.py
+++ merge_training_data.py
@@ -12,10 +12,10 @@ if __name__ == '__main__':
     parser.add_argument('input', nargs='+', help="Specify input files to merge")
     args = parser.parse_args()
 
-    data = training_data.training_data()
+    data = training_data.training_data(True)
 
     for i in args.input:
-        di = training_data.training_data()
+        di = training_data.training_data(True)
         di.import_csv(i)
         data.merge(di)
 
