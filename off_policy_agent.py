9ee8ab04c6596796238c0af986833dede580a355
Russell Gallop <russell.gallop@gmail.com>
2019-01-12 18:21:47 +0000

    Define reward and board scaling
    Scale board for training and example for testing

--- off_policy_agent.py
+++ off_policy_agent.py
@@ -24,9 +24,15 @@ def train(estimator, replay_memory, gamma=0.9, iterations=100, minibatch_size=64
     """Train estimator using replay_memory"""
     #print("Training")
 
+    board_myu = 1.9
+    board_sigma = 4.2
+    reward_myu = 10.0
+    reward_sigma = 10.0
+
     for i in range(iterations):
         print("Iteration {}".format(i))
         example = np.array([[2, 0, 0, 2], [8, 8, 0, 0], [2, 16, 2, 0], [4, 4, 4, 4]]).reshape((1, 4, 4))
+        example = (example - board_myu) / board_sigma
         prediction_2d = deep_model.get_predictions(estimator, example)
         predictions = prediction_2d.reshape((4))
         for i, v in enumerate(predictions):
@@ -45,14 +51,12 @@ def train(estimator, replay_memory, gamma=0.9, iterations=100, minibatch_size=64
         sample_next_states = sample_data.get_next_x() # (batch_size, 4, 4)
         max_next_prediction = deep_model.get_maxq_per_state(estimator, sample_next_states)
 
-        myu = 10.0
-        sigma = 10.0
         # print("sample_rewards")
         # print(sample_rewards)
         # print("max_next_prediction")
         # print(max_next_prediction)
         # Max prediction comes out normalized so denormalize it
-        max_next_prediction = max_next_prediction * sigma + myu
+        max_next_prediction = max_next_prediction * reward_sigma + reward_myu
         # print("scaled up max_next_prediction")
         # print(max_next_prediction)
         # print("gamma * max_next_prediction")
@@ -62,14 +66,17 @@ def train(estimator, replay_memory, gamma=0.9, iterations=100, minibatch_size=64
         # print("target")
         # print(target)
         # Target all at game score scale, normalize to help training
-        target = (target - myu) / sigma
+        target = (target - reward_myu) / reward_sigma
         # print("normalized target")
         # print(target)
 
         #print(sample_data.get_x())
         #print(sample_data.get_y_digit())
         #print(target)
-        train_input_fn = deep_model.numpy_train_fn(sample_data.get_x(), sample_data.get_y_digit(), target)
+        states = sample_data.get_x()
+        scaled_states = (states - board_myu) / board_sigma
+        #train_input_fn = deep_model.numpy_train_fn(sample_data.get_x(), sample_data.get_y_digit(), target)
+        train_input_fn = deep_model.numpy_train_fn(scaled_states, sample_data.get_y_digit(), target)
         estimator.train(input_fn=train_input_fn)
 
         print("")
