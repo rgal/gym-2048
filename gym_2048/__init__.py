16d015f1057a658680e05f1cbd5bb77a71ea7b64
Russell Gallop <russell.gallop@gmail.com>
2019-05-30 23:18:08 +0100

    Remove timestep_limit parameter from register, no longer supported.

--- gym_2048/__init__.py
+++ gym_2048/__init__.py
@@ -2,6 +2,5 @@ from gym.envs.registration import register
 
 register(
     id='2048-v0',
-    entry_point='gym_2048.envs:Game2048Env',
-    timestep_limit=1000,
+    entry_point='gym_2048.envs:Game2048Env'
 )
