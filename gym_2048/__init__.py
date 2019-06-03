9d504eeba999d544b1149a75e6c6ac586e49ca75
Russell Gallop <russell.gallop@gmail.com>
2019-06-03 19:10:19 +0100

    Revert "Remove timestep_limit parameter from register, no longer supported."
    This reverts commit 16d015f1057a658680e05f1cbd5bb77a71ea7b64.

--- gym_2048/__init__.py
+++ gym_2048/__init__.py
@@ -2,5 +2,6 @@ from gym.envs.registration import register
 
 register(
     id='2048-v0',
-    entry_point='gym_2048.envs:Game2048Env'
+    entry_point='gym_2048.envs:Game2048Env',
+    timestep_limit=1000,
 )
