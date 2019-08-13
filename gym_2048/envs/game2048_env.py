21a735bb6ab7fbf393c04d115b4cf9dc14be6ea7
Russell Gallop <russell.gallop@gmail.com>
2019-08-13 09:06:32 +0100

    Fix shape to be 16 stacked and flat board

--- gym_2048/envs/game2048_env.py
+++ gym_2048/envs/game2048_env.py
@@ -52,7 +52,7 @@ class Game2048Env(gym.Env):
         self.action_space = spaces.Discrete(4)
         # Suppose that the maximum tile is as if you have powers of 2 across the board.
         layers = self.squares
-        self.observation_space = spaces.Box(0, 1, (self.w, self.h, layers), dtype=np.int)
+        self.observation_space = spaces.Box(0, 1, (self.w * self.h, layers), dtype=np.int)
         self.set_illegal_move_reward(0.)
 
         # Initialise seed
@@ -92,9 +92,8 @@ class Game2048Env(gym.Env):
             reward = self.illegal_move_reward
 
         #print("Am I done? {}".format(done))
-        observation = self.Matrix.flatten()
         info = dict()
-        return stack(observation), reward, done, info
+        return stack(self.Matrix.flatten()), reward, done, info
         # Return observation (board state), reward, done and info dict
 
     def reset(self):
