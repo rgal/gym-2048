cef4b6c8f24c8c7d9f70b853f1ee9bd7dce46c51
Russell Gallop <russell.gallop@gmail.com>
2019-09-15 14:19:27 +0100

    Bump gym_2048 version to 0.0.4
    Returned state now a stack
    Defaults to no high tile limit

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup
 
 setup(name='gym_2048',
-      version='0.0.3',
+      version='0.0.4',
       install_requires=['gym'],
       packages=['gym_2048', 'gym_2048.envs'],
       url='https://github.com/rgal/gym-2048',
