85d7ceea0840c137d14d74c0736b3572069ba64d
Russell Gallop <russell.gallop@gmail.com>
2019-10-02 21:48:46 +0100

    Add dqn_tutorial based on tf agents notebook

---
+++ dqn_tutorial.py
@@ -0,0 +1,258 @@
+from __future__ import absolute_import, division, print_function
+
+import base64
+import imageio
+#import IPython
+#import matplotlib
+#import matplotlib.pyplot as plt
+#import PIL.Image
+#import pyvirtualdisplay
+
+import tensorflow as tf
+
+from tf_agents.agents.dqn import dqn_agent
+from tf_agents.drivers import dynamic_step_driver
+from tf_agents.environments import suite_gym
+from tf_agents.environments import tf_py_environment
+from tf_agents.eval import metric_utils
+from tf_agents.metrics import tf_metrics
+from tf_agents.networks import q_network
+from tf_agents.policies import random_tf_policy
+from tf_agents.replay_buffers import tf_uniform_replay_buffer
+from tf_agents.trajectories import trajectory
+from tf_agents.utils import common
+
+import gym_2048
+
+tf.compat.v1.enable_v2_behavior()
+
+
+# Set up a virtual display for rendering OpenAI gym environments.
+#display = pyvirtualdisplay.Display(visible=0, size=(1400, 900)).start()
+
+num_iterations = 20000 # @param {type:"integer"}
+
+initial_collect_steps = 1000  # @param {type:"integer"} 
+collect_steps_per_iteration = 1  # @param {type:"integer"}
+replay_buffer_max_length = 100000  # @param {type:"integer"}
+
+batch_size = 64  # @param {type:"integer"}
+learning_rate = 1e-3  # @param {type:"number"}
+log_interval = 200  # @param {type:"integer"}
+
+num_eval_episodes = 10  # @param {type:"integer"}
+eval_interval = 1000  # @param {type:"integer"}
+
+env_name = '2048-v0'
+env = suite_gym.load(env_name)
+
+#@test {"skip": true}
+env.reset()
+#PIL.Image.fromarray(env.render())
+
+print('Observation Spec:')
+print(env.time_step_spec().observation)
+
+print('Reward Spec:')
+print(env.time_step_spec().reward)
+
+print('Action Spec:')
+print(env.action_spec())
+
+time_step = env.reset()
+print('Time step:')
+print(time_step)
+
+action = 1
+
+next_time_step = env.step(action)
+print('Next time step:')
+print(next_time_step)
+
+train_py_env = suite_gym.load(env_name)
+eval_py_env = suite_gym.load(env_name)
+
+train_env = tf_py_environment.TFPyEnvironment(train_py_env)
+eval_env = tf_py_environment.TFPyEnvironment(eval_py_env)
+
+fc_layer_params = (2048, 2048, 2048, 2048)
+
+q_net = q_network.QNetwork(
+    train_env.observation_spec(),
+    train_env.action_spec(),
+    fc_layer_params=fc_layer_params)
+
+optimizer = tf.compat.v1.train.AdamOptimizer(learning_rate=learning_rate)
+
+train_step_counter = tf.Variable(0)
+
+agent = dqn_agent.DqnAgent(
+    train_env.time_step_spec(),
+    train_env.action_spec(),
+    q_network=q_net,
+    optimizer=optimizer,
+    td_errors_loss_fn=common.element_wise_squared_loss,
+    train_step_counter=train_step_counter)
+
+agent.initialize()
+
+eval_policy = agent.policy
+collect_policy = agent.collect_policy
+
+random_policy = random_tf_policy.RandomTFPolicy(train_env.time_step_spec(),
+                                                train_env.action_spec())
+
+example_environment = tf_py_environment.TFPyEnvironment(
+    suite_gym.load('2048-v0'))
+
+time_step = example_environment.reset()
+
+random_policy.action(time_step)
+
+#@test {"skip": true}
+def compute_avg_return(environment, policy, num_episodes=10):
+
+  total_return = 0.0
+  for _ in range(num_episodes):
+
+    time_step = environment.reset()
+    episode_return = 0.0
+
+    while not time_step.is_last():
+      action_step = policy.action(time_step)
+      time_step = environment.step(action_step.action)
+      episode_return += time_step.reward
+    total_return += episode_return
+
+  avg_return = total_return / num_episodes
+  return avg_return.numpy()[0]
+
+
+# See also the metrics module for standard implementations of different metrics.
+# https://github.com/tensorflow/agents/tree/master/tf_agents/metrics
+
+compute_avg_return(eval_env, random_policy, num_eval_episodes)
+
+replay_buffer = tf_uniform_replay_buffer.TFUniformReplayBuffer(
+    data_spec=agent.collect_data_spec,
+    batch_size=train_env.batch_size,
+    max_length=replay_buffer_max_length)
+
+agent.collect_data_spec
+agent.collect_data_spec._fields
+
+#@test {"skip": true}
+def collect_step(environment, policy, buffer):
+  time_step = environment.current_time_step()
+  action_step = policy.action(time_step)
+  next_time_step = environment.step(action_step.action)
+  traj = trajectory.from_transition(time_step, action_step, next_time_step)
+
+  # Add trajectory to the replay buffer
+  buffer.add_batch(traj)
+
+def collect_data(env, policy, buffer, steps):
+  for _ in range(steps):
+    collect_step(env, policy, buffer)
+
+collect_data(train_env, random_policy, replay_buffer, steps=100)
+
+# This loop is so common in RL, that we provide standard implementations. 
+# For more details see the drivers module.
+# https://github.com/tensorflow/agents/blob/master/tf_agents/docs/python/tf_agents/drivers.md
+
+# For the curious:
+# Uncomment to peel one of these off and inspect it.
+# iter(replay_buffer.as_dataset()).next()
+
+# Dataset generates trajectories with shape [Bx2x...]
+dataset = replay_buffer.as_dataset(
+    num_parallel_calls=3, 
+    sample_batch_size=batch_size, 
+    num_steps=2).prefetch(3)
+
+
+dataset
+
+iterator = iter(dataset)
+
+print(iterator)
+
+# For the curious:
+# Uncomment to see what the dataset iterator is feeding to the agent.
+# Compare this representation of replay data 
+# to the collection of individual trajectories shown earlier.
+
+# iterator.next()
+
+#@test {"skip": true}
+#%%time
+
+# (Optional) Optimize by wrapping some of the code in a graph using TF function.
+agent.train = common.function(agent.train)
+
+# Reset the train step
+agent.train_step_counter.assign(0)
+
+# Evaluate the agent's policy once before training.
+avg_return = compute_avg_return(eval_env, agent.policy, num_eval_episodes)
+returns = [avg_return]
+
+for _ in range(num_iterations):
+
+  # Collect a few steps using collect_policy and save to the replay buffer.
+  for _ in range(collect_steps_per_iteration):
+    collect_step(train_env, agent.collect_policy, replay_buffer)
+
+  # Sample a batch of data from the buffer and update the agent's network.
+  experience, unused_info = next(iterator)
+  train_loss = agent.train(experience).loss
+
+  step = agent.train_step_counter.numpy()
+
+  if step % log_interval == 0:
+    print('step = {0}: loss = {1}'.format(step, train_loss))
+
+  if step % eval_interval == 0:
+    avg_return = compute_avg_return(eval_env, agent.policy, num_eval_episodes)
+    print('step = {0}: Average Return = {1}'.format(step, avg_return))
+    returns.append(avg_return)
+
+#@test {"skip": true}
+
+iterations = range(0, num_iterations + 1, eval_interval)
+#plt.plot(iterations, returns)
+#plt.ylabel('Average Return')
+#plt.xlabel('Iterations')
+#plt.ylim(top=250)
+
+#def embed_mp4(filename):
+#  """Embeds an mp4 file in the notebook."""
+#  video = open(filename,'rb').read()
+#  b64 = base64.b64encode(video)
+#  tag = '''
+#  <video width="640" height="480" controls>
+#    <source src="data:video/mp4;base64,{0}" type="video/mp4">
+#  Your browser does not support the video tag.
+#  </video>'''.format(b64.decode())
+#
+#  return IPython.display.HTML(tag)
+
+#def create_policy_eval_video(policy, filename, num_episodes=5, fps=30):
+#  filename = filename + ".mp4"
+#  with imageio.get_writer(filename, fps=fps) as video:
+#    for _ in range(num_episodes):
+#      time_step = eval_env.reset()
+#      video.append_data(eval_py_env.render())
+#      while not time_step.is_last():
+#        action_step = policy.action(time_step)
+#        time_step = eval_env.step(action_step.action)
+#        video.append_data(eval_py_env.render())
+#  return embed_mp4(filename)
+#
+#
+#
+#
+#create_policy_eval_video(agent.policy, "trained-agent")
+#
+#create_policy_eval_video(random_policy, "random-agent")
