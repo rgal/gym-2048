1a31a2694b9bdb907c5dd27298a153401e253c84
Russell Gallop <russell.gallop@gmail.com>
2022-03-19 13:53:43 +0000

    Add --output-model option to gather_training_data.py
    This lets you do some training, save model results and training data, then resume later.

--- gather_training_data.py
+++ gather_training_data.py
@@ -224,9 +224,13 @@ if __name__ == '__main__':
     parser = argparse.ArgumentParser()
     parser.add_argument('--input', '-i', default=None, help="Input existing training data to start from (optional)")
     parser.add_argument('--model', '-m', default=None, help="Pre-trained model to start from (optional)")
-    parser.add_argument('--output', '-o', default='data_{}.csv'.format(int(time.time())), help="Set the output training data file name")
-    parser.add_argument('--results', '-r', default='results_{}.json'.format(int(time.time())), help="Set the output results file name")
     parser.add_argument('--reload-results', default=None, help="Reload previous results")
+
+    timestamp = int(time.time())
+    parser.add_argument('--output', '-o', default='data_{}.csv'.format(timestamp), help="Set the output training data file name")
+    parser.add_argument('--output-model', default='model_{}.hdf5'.format(timestamp), help="Set the output model file name")
+    parser.add_argument('--results', '-r', default='results_{}.json'.format(timestamp), help="Set the output results file name")
+
     parser.add_argument('--seed', type=int, default=None, help="Set the seed for the game")
     args = parser.parse_args()
     # Initialise environment
@@ -301,3 +305,6 @@ if __name__ == '__main__':
 
     if alldata.size():
         alldata.export_csv(args.output)
+
+    if args.output_model:
+        model.save(args.output_model)
